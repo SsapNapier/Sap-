9f2418335212c69445d932dbeea91c983e4b8e02
SsapNapier <48889205+SsapNapier@users.noreply.github.com>
2019-03-28 12:47:41 +0200

    Add files via upload

---
+++ components/HeaderBar.vue
@@ -0,0 +1,89 @@
+<template>
+<div class="header-bar" id="menu">
+  <nuxt-link class="header-bar link active" to="/">Главная</nuxt-link>
+  <nuxt-link class="header-bar link" to="/okompanii">О компании</nuxt-link>
+  <nuxt-link class="header-bar link" to="/statsubagentom">Стать субагентом</nuxt-link>
+  <nuxt-link class="header-bar link" to="/uslygi">Услуги</nuxt-link>
+  <nuxt-link class="header-bar link" to="/kontakty">Контакты</nuxt-link>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+* {
+  box-sizing: border-box; }
+
+#menu {
+  margin-top: 89px; }
+  @media screen and (max-width: 1280px) {
+    #menu {
+      margin-top: 84px; } }
+  #menu nuxt-link {
+    width: 25%;
+    height: 19px;
+    font-family: SegoeUI;
+    font-size: 12px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.36;
+    letter-spacing: normal;
+    text-align: left;
+    color: #c5c5c5;
+    margin-right: 3%;
+    cursor: pointer; }
+    @media screen and (max-width: 1280px) {
+      #menu nuxt-link {
+        font-family: SegoeUI;
+        font-size: 11px;
+        font-weight: normal;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: 1.36;
+        letter-spacing: normal;
+        color: #c5c5c5;
+        margin-right: 3%;
+        cursor: pointer;
+        display: inline-flex;
+        flex-wrap: wrap;
+        width: auto; } }
+  #menu :hover {
+    color: #00aeef;
+    border-bottom: 1px solid #00aeef;
+    padding-bottom: 14px;
+    text-decoration: none; }
+  #menu .active {
+    color: #00aeef;
+    border-bottom: 1px solid #00aeef;
+    padding-bottom: 14px;
+    text-decoration: none; }
+
+#menu:before {
+  content: '';
+  display: inline-block;
+  vertical-align: middle;
+  box-sizing: border-box;
+  width: 37%;
+  margin-right: 60px;
+  height: 1px;
+  background: #c5c5c5; }
+  @media screen and (max-width: 1280px) {
+    #menu:before {
+      content: '';
+      display: inline-block;
+      vertical-align: middle;
+      box-sizing: border-box;
+      width: 100%;
+      height: 1px;
+      background: #c5c5c5;
+      position: relative;
+      left: -0px;
+      margin-left: -798px;
+      display: inline-flex; } }
+
+body {
+  background: #1a1a1a;
+  margin: 0 auto; }
+</style>
---
+++ components/Sidebar.vue
@@ -0,0 +1,94 @@
+<template>
+<div class="sidebar">
+ <a class="active">Войти&nbsp;&nbsp;&nbsp;&#8594;</a>
+ <div class="bar">
+   <a>Авиабилеты</a>
+   <hr>
+   <a>Ж/Д Авиабилеты</a>
+   <hr>
+   <a>Гостиницы</a>
+   <hr>
+   <a>Страхование</a>
+   <hr>
+   <a>Трансфер</a>
+   <hr>
+   <a>Туризм</a>
+ </div>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+.sidebar {
+  position: sticky;
+  left: 1712px;
+  width: 224px;
+  height: 1272px;
+  font-family: SegoeUI;
+  font-size: 12px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 5.22;
+  letter-spacing: normal;
+  color: #c5c5c5;
+  border-left: 1px solid #c5c5c5;
+  margin-top: -100px;
+  padding-left: 46px;
+  padding-top: 68px; }
+  @media screen and (max-width: 1280px) {
+    .sidebar {
+      position: sticky;
+      left: 1103px;
+      width: 175px;
+      height: 1272px;
+      font-family: SegoeUI;
+      font-size: 12px;
+      font-weight: normal;
+      font-style: normal;
+      font-stretch: normal;
+      line-height: 5.22;
+      letter-spacing: normal;
+      color: #c5c5c5;
+      border-left: 1px solid #c5c5c5;
+      margin-top: -104px;
+      padding-left: 39px;
+      padding-top: 61px; } }
+  .sidebar .bar {
+    margin-top: 134px;
+    vertical-align: right; }
+    @media screen and (max-width: 1280px) {
+      .sidebar .bar {
+        margin-top: 134px;
+        flex-direction: column;
+        margin-left: -8px; } }
+  .sidebar hr {
+    margin-left: 0px;
+    margin-top: 4px;
+    margin-bottom: 4px;
+    width: 10px; }
+    @media screen and (max-width: 1280px) {
+      .sidebar hr {
+        margin-left: -0px;
+        margin-top: -3px;
+        margin-bottom: -3px;
+        width: 10px; } }
+  .sidebar a {
+    padding: -10px; }
+    @media screen and (max-width: 1280px) {
+      .sidebar a {
+        padding: -10px; } }
+  .sidebar .active {
+    color: #00aeef;
+    padding-bottom: 14px;
+    text-decoration: none;
+    cursor: pointer; }
+    @media screen and (max-width: 1280px) {
+      .sidebar .active {
+        color: #00aeef;
+        padding-bottom: 14px;
+        text-decoration: none;
+        cursor: pointer; } }
+</style>
